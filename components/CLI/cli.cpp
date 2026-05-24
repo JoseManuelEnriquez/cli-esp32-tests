eae507be7f2c88c45ad23c389ba2117aa3389fed
Jose Manuel Enriquez Baena <joseenriquezbaena@gmail.com>
2026-05-24 10:32:49 +0200

    feat: Parser de tests

--- components/CLI/cli.cpp
+++ components/CLI/cli.cpp
@@ -11,9 +11,25 @@ void CLI::receive_command(){
     uint32_t len = uart.receive(buf, size_buf);
     if(len > 0){
         std::string_view cmd(reinterpret_cast<const char*>(buf), len);
-        // tengo que hacer el parser
+        if (cmd.empty()) return;
+
+        size_t space_pos = cmd.find(' ');
         std::string_view name;
         std::string_view parameters;
+        
+        if (space_pos == std::string_view::npos) {
+            name = cmd;
+            parameters = "";
+        }else {
+            name = cmd.substr(0, space_pos);
+            parameters = cmd.substr(space_pos + 1);
+            
+            size_t first_not_space = parameters.find_first_not_of(' ');
+            if (first_not_space != std::string_view::npos) {
+                parameters = parameters.substr(first_not_space);
+            }
+        }
+
         invoker.process(name, parameters);
     }
 }
